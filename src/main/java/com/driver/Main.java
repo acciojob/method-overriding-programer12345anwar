5a6d511146811736405667cc99e7c24bcf7f6444
programer12345anwar <mdanwar40212@gmail.com>
2025-01-12 19:08:40 +0530

    updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,30 @@
 package com.driver;
 
-class A{
-    public String meth(){ 
-        return "Invoking method from class A"; 
-    }//abstact method must be inside abstract class
+// Task 1: Create class A
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
 }
 
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
+// Task 2: Create class B extending A
+class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
     }
 }
 
 public class Main {
     public static void main(String[] args) {
+        // Task 3: Create an object of class B
         B b = new B();
+
+        // Call method meth from class A using object of B
+        A a = new B(); // Polymorphism
+        System.out.println(a.meth()); // Calls overridden method in B
+
+        // Task 4 & 5: Call the overridden method from object of class B
         System.out.println(b.meth());
     }
-  
 }
